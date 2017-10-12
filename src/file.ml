4dda9019bb9c2a00406af283dec33e1623ca4378
Santeri Hiltunen <accounts.github@me.hilzu.moe>
2017-10-13 01:12:15 +0300

    Add GPLv3 license notice to top of all source files
    This is recommended by the license

--- src/file.ml
+++ src/file.ml
@@ -1,3 +1,21 @@
+(*
+  node-shim - Utility to manage and use versions of node, npm and yarn
+  Copyright (C) 2017-Present  Santeri Hiltunen
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
 module String = StringLabels
 
 exception Not_a_directory of string
