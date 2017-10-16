9dc77edbc430d1d2b0f672f519f0b08facfbbe7e
Santeri Hiltunen <accounts.github@me.hilzu.moe>
2017-10-16 23:42:25 +0300

    BSD and GNU compatible sed command

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,7 @@ cp main "${INSTALL_PATH}/node-shim"
 for PROGRAM in node npm yarn
 do
   TARGET="${INSTALL_PATH}/$PROGRAM"
-  cp scripts/run-shim-template.sh $TARGET
-  sed -i '' "s/\\\$PROGRAM_PLACEHOLDER/$PROGRAM/" $TARGET
+  cp scripts/run-shim-template.sh "$TARGET"
+  sed -i.bak "s/\\\$PROGRAM_PLACEHOLDER/$PROGRAM/" "$TARGET"
+  rm "$TARGET.bak"
 done
