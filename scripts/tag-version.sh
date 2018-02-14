9015343276117f643bbe55fa9fe107ca4d647aff
Santeri Hiltunen <accounts.github@me.hilzu.moe>
2018-02-14 16:19:24 +0200

    Tag correct file with the new version

--- scripts/tag-version.sh
+++ scripts/tag-version.sh
@@ -11,7 +11,7 @@ if [[ -z "$VERSION" ]]; then
   exit 1
 fi
 
-sed -i.bak "s/^version: \"[0-9.]*\"/version: \"$VERSION\"/" opam
-rm opam.bak
+sed -i.bak "s/^version: \"[0-9.]*\"/version: \"$VERSION\"/" node-shim.opam
+rm node-shim.opam.bak
 git commit -a -m "v$VERSION"
 git tag -a "v$VERSION" -m "v$VERSION"
